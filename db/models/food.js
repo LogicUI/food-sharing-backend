3a0317a26a65879d1d0def3b9d5ea6eaa2c60791
perrylzx <perrylzx@gmail.com>
2020-07-21 21:18:41 +0800

    created food and user models

---
+++ db/models/food.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Food extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Food.belongsTo(models.User)
+    }
+  };
+  Food.init({
+    name: DataTypes.STRING,
+    image: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Food',
+  });
+  return Food;
+};
