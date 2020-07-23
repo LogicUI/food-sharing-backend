041d071614d40a9e73bc3f85295be49d8219b4c3
perrylzx <perrylzx@gmail.com>
2020-07-23 20:59:40 +0800

    deleted test api

--- src/app.js
+++ src/app.js
@@ -19,9 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use('/', indexRouter);
 app.use('/food', foodRouter)
-app.get('/hello', (req, res) => {
-  res.send('hello')
-})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
