df1f48423b8a2ca798f357c2c6df229a2f151717
john <john.wee@flex-solver.com.sg>
2020-07-27 20:30:05 +0800

    update app.js add verifyjwttoken middleware

--- src/app.js
+++ src/app.js
@@ -1,23 +1,22 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-import userRouter from './routes/foodRouter'
+const userRouter = require("./routes/userRouter");
+const verifyJwtToken = require("./middleware/verifyJwtToken");
 
 var app = express();
+
 
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/user', userRouter)
+app.use("/users",userRouter);
+
+app.use(verifyJwtToken);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
