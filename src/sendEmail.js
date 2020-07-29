f24d232d43850bc6f1b48d05ad037b218434a6f5
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-07-30 05:28:23 +0800

    put the secret key in an env var

--- src/sendEmail.js
+++ src/sendEmail.js
@@ -2,9 +2,7 @@ const sgMail = require("@sendgrid/mail");
 
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-sgMail.setApiKey(
-  "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"
-);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
   to: "perry_lzx@outlook.com",
   from: "perrylzx@gmail.com",
