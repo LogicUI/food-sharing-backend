48963b9775337997bd5feded2afb0d79c1a18e16
john <john.wee@flex-solver.sg>
2020-07-30 21:14:03 +0800

    add register validation schema using joi

---
+++ src/JoiValidation/registerValidation.js
@@ -0,0 +1,17 @@
+
+
+const Joi = require('joi-plus');
+
+
+const registerSchema = Joi.object({
+	username: Joi.string().required(),
+	password: Joi.string().password({
+		min: 8,
+		lowercase: true,
+		uppercase: true,
+	}),
+    email: Joi.string().required().email()
+});
+
+
+module.exports = registerSchema;
