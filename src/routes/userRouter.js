8bec52fb953576d0988d15f930a04195f4f0f18c
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-08-05 04:17:19 +0800

    added a verify user route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,10 @@
-const express =  require("express");
+const express = require("express");
 const userController = require("../controllers/userController");
 const router = express.Router();
 const registerValidator = require("../middleware/registerValidator");
 
+router.post("/", registerValidator, userController.registerUser);
 
-router.post('/', registerValidator, userController.registerUser)
-
+router.put("/", userController.verifyUser);
 
 module.exports = router;
