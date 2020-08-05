02f4ab326e10836a213dff6dd052ac2c9bfceceb
perrylzx <perrylzx@gmail.com>
2020-08-05 19:47:38 +0800

    added a login route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,8 +3,11 @@ const userController = require("../controllers/userController");
 const router = express.Router();
 const registerValidator = require("../middleware/registerValidator");
 
+
 router.post("/", registerValidator, userController.registerUser);
 
 router.put("/", userController.verifyUser);
+
+router.post('/login', userController.userLogin)
 
 module.exports = router;
